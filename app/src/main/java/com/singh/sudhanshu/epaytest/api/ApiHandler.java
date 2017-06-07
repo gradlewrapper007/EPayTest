fe2f638d77a4fe43508a8d7ab21b076ba6f5dcdc
Sudhanshu <sudhanshu@snaplion.com>
2017-06-07 07:36:21 +0530

    additional commit

--- app/src/main/java/com/singh/sudhanshu/epaytest/api/ApiHandler.java
+++ app/src/main/java/com/singh/sudhanshu/epaytest/api/ApiHandler.java
@@ -28,7 +28,7 @@ public class ApiHandler {
     // Instantiate the RequestQueue.
 
     public static void fetchTokenAndSaveIfNull(Context ctx, final AppCallback callback) {
-        getRequest(ctx, APIs.LOGIN_URL, new AppCallback() {
+        postRequest(ctx, APIs.LOGIN_URL, new AppCallback() {
 
             @Override
             public void onSuccess(Object data) {
@@ -53,7 +53,58 @@ public class ApiHandler {
         });
     }
 
+    /**
+     * Fetches Get request
+     * @param ctx
+     * @param url
+     * @param callback
+     */
     public static void getRequest(Context ctx, final String url, final AppCallback callback) {
+        RequestQueue queue = Volley.newRequestQueue(ctx);
+        // Request a string response from the provided URL.
+        JSONObject params = new JSONObject();
+        try {
+            params.put("Accept", "application/json");
+            params.put("Content-Type", "application/json");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        Log.i(TAG, "url: " + url);
+                        Log.i(TAG, "onResponse: " + response);
+                        callback.onSuccess(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "url: " + url);
+                Log.i(TAG, "onResponse: " + error.getMessage());
+                callback.onFailure(error.getMessage());
+            }
+        });
+
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+// Add the request to the RequestQueue.
+        queue.add(request);
+    }
+
+    public static void postRequest(Context ctx, final String url, final AppCallback callback) {
         RequestQueue queue = Volley.newRequestQueue(ctx);
         // Request a string response from the provided URL.
         JSONObject params = new JSONObject();
@@ -97,7 +148,6 @@ public class ApiHandler {
 // Add the request to the RequestQueue.
         queue.add(request);
     }
-
     private void appenedHeader() {
 
     }
